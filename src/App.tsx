22caa29ba655aa2b2ffd4a716631c70ad8cebec9
SamuelX <samxiezx1115@gmail.com>
2021-10-07 14:45:41 -0400

    weather panel creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React,{useState,useEffect} from 'react';
 
+import WeatherData from './services/forecast';
+import { Weather as WeatherInfo } from './services/forecast';
+
 import './App.css';
 
 
@@ -7,8 +10,7 @@ import './App.css';
 function App() {
 
   const [cityName,setCityName]=useState<string>('')
-  const [weather,setWeather]=useState([])
-
+  const [weather,setWeather]=useState<WeatherInfo[]>([])
   const handleChange=(e:any):void=>{
       setCityName(e.target.value)
   } 
@@ -21,6 +23,20 @@ function App() {
         console.log('Submit button triggered')
         console.log(cityName)
         //trigger service here
+        WeatherData(cityName).then((res):void=>{
+          console.log(res)
+          let future_temperature = res.list[5].main
+          let future_weather  = res.list[5].weather[0].main
+          setWeather(weather.concat(
+            {
+              city:cityName,
+              temperature:future_temperature,
+              weather:future_weather
+            }
+          ))
+        }).catch((err:any)=>{
+          console.log(err)
+        })
       }
   }
 
@@ -34,8 +50,9 @@ function App() {
             <button>Add</button>
           </form>
       </div>
-      <div className=''>
-
+      {/* listed by city */}
+      <div className='weather-panel'>
+        
       </div>
     </div>
   );
---
+++ src/services/forecast.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export interface Weather {
+    city:string,
+    temperature:number
+    weather?:string
+}
+
+const WeatherData = async (cityname:string):Promise<any>=>{
+    let api_key = '2357e9d6edbc1dca9778ffaae19a1bf0'
+    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=${api_key}`
+
+    let data = await fetch(url,{
+        method:'GET'
+    })
+
+    return data.json()
+}
+
+export default WeatherData
